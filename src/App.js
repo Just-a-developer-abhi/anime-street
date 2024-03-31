0399777cccbde439dc552803f025d1f25e2c1789
Just-a-developer-abhi <abhi344741@gmail.com>
2024-03-31 23:35:23 +0530

    main component push

--- src/App.js
+++ src/App.js
@@ -1,38 +1,50 @@
 import { ReactDOM, useEffect, useState } from "react";
-import './assets/scss/main.css';
+import "./assets/scss/main.css";
 import Header from "./component/Header";
 import Sidebar from "./component/Sidebar";
 import MainContent from "./component/MainContent";
 
-
 function App() {
-  const [animeList, setAnimeList]= useState([]);
+  const [animeList, setAnimeList] = useState([]);
   const [topAnime, setTopAnime] = useState([]);
-  const [search, seatSearch] = useState("");
+  const [search, setSearch] = useState("");
 
   const GetTopAnime = async () => {
-    const getTopList = await fetch(`https://api.jikan.moe/v4/top/anime`).then(res=>
-      res.json()
-      );
-    console.log("response  "+ JSON.stringify(getTopList))
-    setTopAnime(getTopList.data.slice(0,5));
-  }
+    const getTopList = await fetch(`https://api.jikan.moe/v4/top/anime`).then(
+      (res) => res.json()
+    );
+    // console.log("response  " + JSON.stringify(getTopList));
+    setTopAnime(getTopList.data.slice(0, 5));
+    setAnimeList(getTopList.data);
+  };
 
-  useEffect(()=>{
+  // const FetchAnime= async () =>{
+  //   const searchAnime = await fetch(`https://api.jikan.moe
+  // }
+  const HandleSearch = (e) => {
+    e.preventDefault();
+    // console.log(search + " is the search");
+  };
+
+  useEffect(() => {
     GetTopAnime();
+  }, []);
 
-  },[]);
-
-  console.log("anime list : " + JSON.stringify(animeList));
+  // console.log("anime list : " + JSON.stringify(animeList));
   return (
     <div className="App">
-    <Header/>
-    <div className="content-wrap">
-        <Sidebar topAnime={topAnime}/>
-        <MainContent/>
+      <Header />
+      <div className="content-wrap">
+        <Sidebar topAnime={topAnime} />
+        <MainContent
+          HandleSearch={HandleSearch}
+          search={search}
+          setSearch={setSearch}
+          animeList={animeList}
+          // FetchAnime={FetchAnime}
+        />
+      </div>
     </div>
-    </div>
-
   );
 }
 
--- src/component/MainContent.js
+++ src/component/MainContent.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React from "react";
+import AnimeCard from "./AnimeCard";
 
-const MainContent = () => {
+const MainContent = (props) => {
   return (
     <main>
-        <div className="main-head">
-            <form 
-            className="search-box">
-                <input 
-                type='search'
-                placeholder='search for any Anime'
-                required
-                />
-            </form>
-        </div>
+      <div className="main-head">
+        <form onSubmit={props.HandleSearch} className="search-box">
+          <input
+            type="search"
+            placeholder="search for any Anime"
+            required
+            value={props.search}
+            onChange={(e) => {
+              props.setSearch(e.target.value);
+            }}
+          />
+        </form>
+      </div>
+      <div className="anime-list">
+        {props.animeList.map((anime) => (
+          <AnimeCard anime={anime} key={anime.mal_id} />
+        ))}
+      </div>
     </main>
-  )
-}
+  );
+};
 
-export default MainContent
+export default MainContent;
